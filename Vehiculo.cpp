0aca38dde6114827b68044410c3908c29689d1b7
RodrigoAndre0512 <rodrigo.ccallo@ucsp.edu.pe>
2025-03-10 18:53:37 -0500

    Update Vehiculo.cpp

--- Vehiculo.cpp
+++ Vehiculo.cpp
@@ -27,16 +27,3 @@ Vehiculo::~Vehiculo()
     delete[] Modelo;
 }
 
-// Método para crear ID
-void Administrador::crearID(const char* dni, const char* nombre, const char* apellido) 
-{
-    ID[0] = 'A';
-    ID[1] = 'D';
-    ID[2] = nombre[0];
-    ID[3] = apellido[0];
-    // Copiamos los primeros 8 caracteres del DNI al ID
-    copiarCaracteres(ID + 4, dni, 8);
-    ID[13] = '\0';
-
-    std::cout << "Tu ID de inicio de sesión es: " << ID << std::endl;
-}
